0b22c58011398cc720fe7705220586115ca4e423
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-01 16:46:23 +0530

    Create exp5-a.cpp

---
+++ exp5-a.cpp
@@ -0,0 +1,26 @@
+//Dhairya Thakkar
+//prn 24070123037
+//A3
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout << "Enter a the number:  ";
+    cin >> a; //input for variable
+    if (a > 0) {//if condition fo +ve number
+    if (a % 2 == 0) {
+        cout << "The number is even." ; //output
+    }
+    else  {
+        cout << "The number is odd." ;
+    }}
+    else{
+        cout << "The number is negative." ;
+    }
+    return 0;
+}
+
+/* output:
+Enter a the number:  5
+The number is odd.
+*/
